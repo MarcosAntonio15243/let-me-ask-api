708043616c9c4fa37e68ddeb7b50b22b23500bff
MarcosAntonio15243 <marcosantonio15243@gmail.com>
2025-07-11 14:18:08 -0300

    feat: integrate Gemini API and add embedding generation for semantic search

---
+++ src/services/gemini.ts
@@ -0,0 +1,49 @@
+import { GoogleGenAI } from '@google/genai';
+import { env } from '../env.ts';
+
+const gemini = new GoogleGenAI({
+  apiKey: env.GEMINI_API_KEY,
+});
+
+const model = 'gemini-2.5-flash';
+
+export async function transcribeAudio(audioAsBase64: string, mimeType: string) {
+  const response = await gemini.models.generateContent({
+    model,
+    contents: [
+      {
+        text: 'Trancreva o áudio para português do Brasil. Seja preciso e natural na transcrição. Mantenha a pontuação adequada e divida o texto em parágrafos quando for apropriado.',
+      },
+      {
+        inlineData: {
+          mimeType,
+          data: audioAsBase64,
+        },
+      },
+    ],
+  });
+
+  if (!response.text) {
+    throw new Error('Não foi possível converter o áudio.');
+  }
+
+  return response.text;
+}
+
+export async function generateEmbeddings(text: string) {
+  const response = await gemini.models.embedContent({
+    model: 'text-embedding-004',
+    contents: [{ text }],
+    config: {
+      // Indica a finalidade para qual o embedding será utilizado
+      // RETRIEVAL_DOCUMENT refere-se a utilizar para buscas semânticas e reuso em outros prompts
+      taskType: 'RETRIEVAL_DOCUMENT',
+    },
+  });
+
+  if (!response.embeddings?.[0].values) {
+    throw new Error('Não foi possível gerar os embeddings.');
+  }
+
+  return response.embeddings[0].values;
+}
